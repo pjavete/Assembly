df6fcc9ff35d6688b005de47ea1de0abc0a75bfe
Payton <paytonjavete@sbcglobal.net>
2018-10-31 17:39:42 -0700

    Files not showing up

--- app/src/main/java/com/example/payton/assembly/database.java
+++ app/src/main/java/com/example/payton/assembly/database.java
@@ -9,7 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.EditText;
 
-
+//Change to Firebase
 public class database extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "people.db";
