9783b11373e59ff0936550e8a3e5c705f2404bb3
chtzou <chtzou@ucsc.edu>
2018-11-02 16:04:43 -0700

    currently activity changes crashes app

--- app/src/main/java/com/example/payton/assembly/myEvents.java
+++ app/src/main/java/com/example/payton/assembly/myEvents.java
@@ -1,4 +1,35 @@
 package com.example.payton.assembly;
 
-public class myEvents {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class myEvents extends AppCompatActivity {
+    Spinner eventSpinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_events);
+        eventSpinner = findViewById(R.id.eventSpinner);
+
+        ArrayAdapter<CharSequence> myAdapter = ArrayAdapter.createFromResource(this, R.array.eventChoices, android.R.layout.simple_spinner_item);
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventSpinner.setAdapter(myAdapter);
+    }
+
+    public void decision(View view) {
+        String choice = eventSpinner.getSelectedItem().toString() + "\n";
+
+    }
 }
