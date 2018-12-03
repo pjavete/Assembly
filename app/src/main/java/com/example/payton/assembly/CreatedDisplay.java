b8f533ea61b04ef4ae275c08cedb353cc9b58a90
Payton Javete <pjavete@ucsc.edu>
2018-12-02 16:38:42 -0800

    finished making all details display

--- app/src/main/java/com/example/payton/assembly/CreatedDisplay.java
+++ app/src/main/java/com/example/payton/assembly/CreatedDisplay.java
@@ -25,9 +25,6 @@ public class CreatedDisplay extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private ArrayList<StringBuffer> titles = new ArrayList<>();
-    private ArrayList<StringBuffer> startDate = new ArrayList<>();
-    private ArrayList<StringBuffer> endDate = new ArrayList<>();
-    private ArrayList<StringBuffer> location = new ArrayList<>();
     private ArrayList<StringBuffer> description = new ArrayList<>();
 
     @Override
@@ -48,31 +45,20 @@ public class CreatedDisplay extends AppCompatActivity {
                 description.clear();
                 for(DocumentSnapshot snapshot : documentSnapshots){
                     StringBuffer titleBuffer = new StringBuffer();
-                    StringBuffer startDateBuffer = new StringBuffer();
-                    StringBuffer endDateBuffer = new StringBuffer();
-                    StringBuffer locationBuffer = new StringBuffer();
                     StringBuffer descriptionBuffer = new StringBuffer();
+
                     titleBuffer.append(snapshot.getString("Event Name"));
-                    /*
                     String Details = "Start Date and Time: " + snapshot.get("Start Date").toString() + "\n"
                             + "End Date and Time: " + snapshot.get("End Date").toString() + "\n"
                             + "Location: " + snapshot.getString("Location") + "\n"
                             + snapshot.getString("Description");
-                    */
-                    startDateBuffer.append("Start Date and Time: " + snapshot.get("Start Date").toString());
-                    endDateBuffer.append("End Date and Time: " + snapshot.get("End Date").toString());
-                    locationBuffer.append("Location: " + snapshot.getString("Location"));
-                    descriptionBuffer.append(snapshot.getString("Description"));
+                    descriptionBuffer.append(Details);
 
                     titles.add(titleBuffer);
-                    startDate.add(startDateBuffer);
-                    endDate.add(endDateBuffer);
-                    location.add(locationBuffer);
                     description.add(descriptionBuffer);
                 }
 
-                //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, titles);
-                lAdapter = new ListAdapter(getApplicationContext(), titles, startDate, endDate, location, description);
+                lAdapter = new ListAdapter(getApplicationContext(), titles, description);
                 lAdapter.notifyDataSetChanged();
                 display.setAdapter(lAdapter);
             }
