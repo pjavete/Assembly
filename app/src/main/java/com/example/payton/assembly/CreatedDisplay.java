7c56b1413d70f2899d7a0a7d83bfc6d1d1e90359
felecia <feleciag1014@gmail.com>
2018-12-05 21:17:05 -0800

    added the nav bar to the created events

--- app/src/main/java/com/example/payton/assembly/CreatedDisplay.java
+++ app/src/main/java/com/example/payton/assembly/CreatedDisplay.java
@@ -1,7 +1,13 @@
 package com.example.payton.assembly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,6 +35,9 @@ public class CreatedDisplay extends AppCompatActivity {
     private ArrayList<StringBuffer> titles = new ArrayList<>();
     private ArrayList<StringBuffer> description = new ArrayList<>();
     private ArrayList<String> eventIDs = new ArrayList<>();
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
 
     @Override
@@ -72,6 +81,47 @@ public class CreatedDisplay extends AppCompatActivity {
                 display.setAdapter(lAdapter);
             }
         });
+        dl = (DrawerLayout) findViewById(R.id.activity_my_events);
+        t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView) findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.created:
+                        Intent intent = new Intent(CreatedDisplay.this, CreatedDisplay.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.joined:
+                        Intent intent2 = new Intent(CreatedDisplay.this, JoinedDisplay.class);
+                        startActivity(intent2);
+                        return true;
+
+                    default:
+                        return true;
+                }
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
 
