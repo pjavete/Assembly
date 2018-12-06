ef75800da9f854ca1c4e2fbe29687cbe0459607f
Payton Javete <pjavete@ucsc.edu>
2018-12-05 20:18:38 -0800

    menu now shows only leave if it is a joined event and edit and delete for a created event. final push for this branch

--- app/src/main/java/com/example/payton/assembly/ListAdapter.java
+++ app/src/main/java/com/example/payton/assembly/ListAdapter.java
@@ -1,10 +1,12 @@
 package com.example.payton.assembly;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +35,8 @@ public class ListAdapter extends BaseAdapter {
     private final ArrayList<StringBuffer> Names;
     private final ArrayList<StringBuffer> Desc;
     private final ArrayList<String> eventIDs;
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
 
     public ListAdapter(Context context, ArrayList<StringBuffer> Names, ArrayList<StringBuffer> Desc, ArrayList<String> eventIDs){
         //super(context, R.layout.single_list__item, utilsArrayList);
@@ -95,49 +99,51 @@ public class ListAdapter extends BaseAdapter {
             public void onClick(View v)
             {
                 final PopupMenu popmenu = new PopupMenu(context, v);
+
                 popmenu.getMenuInflater().inflate(R.menu.popup_menu, popmenu.getMenu());
+                final Menu options = popmenu.getMenu();
 
-                popmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
-                {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.edit:
-                                Intent editPage = new Intent(context, editEvents.class);
-                                editPage.putExtra("editID", eventID);
-                                context.startActivity(editPage);
-                                break;
-                            case R.id.destroy:
-                                FirebaseFirestore db;
-                                FirebaseAuth mAuth;
-                                db = FirebaseFirestore.getInstance();
-                                mAuth = FirebaseAuth.getInstance();
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                final String userID = user.getUid();
+                db = FirebaseFirestore.getInstance();
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser user = mAuth.getCurrentUser();
+                final String userID = user.getUid();
+                Task<DocumentSnapshot> eventTask = db.collection("users").document(userID).collection("myEvents").document(eventID).get();
+                eventTask.addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot snapshot = task.getResult();
+                        final boolean isOwner = (boolean) snapshot.getData().get("Owner");
+                        if(!isOwner){
+                            options.getItem(1).setTitle("Leave");
+                            options.getItem(0).setVisible(false);
+                        }
 
-                                Task<DocumentSnapshot> eventTask = db.collection("users").document(userID).collection("myEvents").document(eventID).get();
-                                eventTask.addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                        DocumentSnapshot snapshot = task.getResult();
-                                        boolean isOwner = (boolean) snapshot.getData().get("Owner");
+                        popmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+                        {
+                            public boolean onMenuItemClick(MenuItem item) {
+                                switch (item.getItemId()) {
+                                    case R.id.edit:
+                                        Intent editPage = new Intent(context, editEvents.class);
+                                        editPage.putExtra("editID", eventID);
+                                        context.startActivity(editPage);
+                                        break;
+                                    case R.id.destroy:
                                         Delete delete = new Delete();
-                                        if(isOwner){
+                                        if (isOwner) {
                                             delete.deleteEvent(userID, eventID);
                                         } else {
                                             delete.leaveEvent(userID, eventID);
                                         }
-                                    }
-                                });
-
-                                break;
-                            default:
-                                break;
-                        }
-                        return true;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                return true;
+                            }
+                        });
+                        popmenu.show();
                     }
                 });
-
-                popmenu.show();
             }
         });
 
